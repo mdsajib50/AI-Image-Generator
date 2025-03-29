581f91851fde130b415ab34601719a834df62621
mdsajib50 <mdajib850@gmail.com>
2025-03-29 16:33:31 +0600

    saved theme mode in localstorage

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const themeToggle = document.querySelector(".theme-toggle");
 
+//Set theme based on saved preference or system default
+(() => {
+    const savedTheme = localStorage.getItem("theme");
+    const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    const isDarkTheme = savedTheme === "dark" || (!savedTheme && systemPrefersDark);
+    document.body.classList.toggle("dark-theme");
+    themeToggle.querySelector("i").className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
+
+})
+
 const toggleTheme = () => {
     const isDarkTheme = document.body.classList.toggle("dark-theme");
+    localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
     themeToggle.querySelector("i").className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
 }
 
